e4a07145c4e0db97b9a74069181f1cbf2451aa6e
thanaporn.ch <thanaporn_chan@cmu.ac.th>
2023-12-24 17:09:25 +0700

    test device view

--- src/api/device.ts
+++ src/api/device.ts
@@ -13,28 +13,38 @@ function detectMimeType(b64: string): string | undefined {
 }
 
 device.get("/device", async (req: Request, res: Response) => {
-  
-  // find information of device in database
-  // const info = await prisma.device.findUnique({
-  //   where: {
-  //     MACaddress: req.query.mac as string,
-  //   },
-  // });
-  // return res.send(info);
+  try {
+    // find information of device in database
+    // const mac = await prisma.device.findUnique({
+    //   where: {
+    //     MACaddress: req.query.mac as string,
+    //   },
+    // });
+    // if (!mac) {
+    //   return res.send({ ok: false, message: "This device doesn't add." });
+    // }
+    // return res.send({ ok: true, mac });
 
-  // test send image to raspberry pi
-  // const data = await prisma.device.findUnique({
-  //   select: { location: true },
-  //   where: {
-  //     MACaddress: req.query.mac as string,
-  //   },
-  // });
-  // if (data?.location) {
-  //   // const image = Buffer.from(data.location, "base64");
-  //   const type = detectMimeType(data.location);
-  //   return res.send(`<img src="data:${type};base64,${data.location}" />`);
-  //   // return res.send(`"data:${type};base64,${data.location}"`);
-  // }
-  // return res.status(404).send({ message: "Image not found" });
-
+    // test send image to raspberry pi
+    const data = await prisma.device.findUnique({
+      select: { location: true },
+      where: {
+        MACaddress: req.query.mac as string,
+      },
+    });
+    if (data?.location) {
+      // const image = Buffer.from(data.location, "base64");
+      const type = detectMimeType(data.location);
+      // return res.send(`<img src="data:${type};base64,${data.location}" />`);
+      return res.send({
+        ok: true,
+        image: `data:${type};base64,${data.location}`,
+      });
+    }
+    return res.status(404).send({ ok: false, message: "Image not found" });
+  } catch (err: any) {
+    return res
+      .status(500)
+      .send({ ok: false, message: "Internal Server Error" });
+  }
 });
